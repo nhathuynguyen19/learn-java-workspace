e289655de1338a687b6eaa9ad95d89f710fbd6e0
nhathuynguyen19 <huythcsthuyphuong73@gmail.com>
2025-04-05 17:59:50 +0700

    loai * khoi git

--- KhoiDau/src/Buoi7/Lop1.java
+++
@@ -1,5 +0,0 @@
-package Buoi7;
-
-public class Lop1 {
-	public static int s = 5;
-}
--- learnjav/src/hello.java
+++
@@ -1,5 +0,0 @@
-public class hello{
-    public static void main(String[] ts){
-        System.out.println("Hello World!");
-    }
-}
