2bbd551ddfd67368c00ab89c09919ba1777424e3
nhathuynguyen19 <huythcsthuyphuong73@gmail.com>
2025-06-12 08:35:59 +0700

    cap nhat cau 3 java co ban

--- valorant/database/BO/SinhVienBo.java
+++ valorant/database/BO/SinhVienBo.java
@@ -3,7 +3,17 @@ package BO;
 import java.util.ArrayList;
 
 import Bean.SinhVien;
+import QuanLyFile.QuanLyFile;
 
 public class SinhVienBo {
-	public ArrayList<SinhVien> danhSachSinhVienNoiSinh(String noiSInh)
+	public QuanLyFile qlf = new QuanLyFile();
+	public ArrayList<SinhVien> danhSachSinhVienNoiSinh(String noiSinh, ArrayList<SinhVien> ds) throws Exception {
+		ArrayList<SinhVien> rs = new ArrayList<SinhVien>();
+		for (SinhVien sv : ds) {
+			if (sv.getNoiSinh().toLowerCase().contains(noiSinh.toLowerCase())) {
+				rs.add(sv);
+			}
+		}
+		return rs;
+	}
 }
--- valorant/database/View/test.java
+++ valorant/database/View/test.java
@@ -1,17 +1,31 @@
 package View;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import BO.SinhVienBo;
+import Bean.SinhVien;
 import QuanLyFile.QuanLyFile;
 
 public class test {
 
 	public static void main(String[] args) {
 		QuanLyFile qlf = new QuanLyFile();
+		Scanner input = new Scanner(System.in);
+		SinhVienBo svbo = new SinhVienBo();
+		ArrayList<SinhVien> ds = new ArrayList<SinhVien>();
 		
 		try {
 			// cau 1
-			qlf.outputDanhSachSinhVienTuDanhSach(qlf.loadSinhVienFromFile("sinhvien.txt"));
+			ds = qlf.loadSinhVienFromFile("sinhvien.txt");
+			qlf.outputDanhSachSinhVienTuDanhSach(ds);
 			
 			// cau 2
+			System.out.print("Nhap noi sinh: ");
+			String noiSinh = input.nextLine();
+			qlf.outputDanhSachSinhVienTuDanhSach(svbo.danhSachSinhVienNoiSinh(noiSinh, ds));
+			
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
